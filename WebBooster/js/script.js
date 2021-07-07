b8f3e379b8372740d5db9d3521ca8d77106e5dd4
Uqatebos <82330231+Uqatebos@users.noreply.github.com>
2021-07-07 03:40:54 +0300

    Add files via upload

---
+++ WebBooster/js/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+// Modal
+
+const modalOpen = document.querySelectorAll('.store-item__button'),
+   modalClose = document.querySelector('[data-close]'),
+   modal = document.querySelector('.modal'),
+   modalContent = document.querySelector('.modal__content'),
+   html = document.querySelector('html');
+
+function openModalWindow() {
+   modalOpen.forEach((item) => {
+      item.addEventListener('click', (event) => {
+         modal.classList.add('show');
+         modal.classList.remove('hide');
+         document.body.style.overflow = 'hidden';
+      })
+   })
+
+}
+
+
+function closeModal() {
+   modal.classList.add('hide');
+   modal.classList.remove('show');
+   document.body.style.overflow = '';
+}
+function closeModalWindow() {
+   modalClose.addEventListener('click', closeModal)
+
+}
+modal.addEventListener('click', (event) => {
+   const target = event.target;
+   if (target === modal) {
+      closeModal();
+   }
+})
+document.addEventListener('keydown', (e) => {
+   if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+   }
+})
+
+openModalWindow();
+closeModalWindow();
